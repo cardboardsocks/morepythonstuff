c0246d2836d87c9361fb33da0d9c9531b985e071
Ben Mckenzie <bmckenzie0522@gmail.com>
2023-11-09 10:34:53 -0700

    intital

---
+++ project/main.py
@@ -0,0 +1,11 @@
+meat = 0
+while meat < 5:
+    print("Meat time boy.")
+    meat = meat + 1
+
+while True:
+    print("What kind of meat do you like?")
+    new_meat = input()
+    if new_meat == "steak":
+        break
+print("Good choice")
